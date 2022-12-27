8d99d1135addeaf5a004928cafbdbc59b8cdb055
ZhykharievAnatolii <nekijt87@gmail.com>
2022-12-27 13:01:09 +0200

    add imports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import {sum} from "./sum";
 document.addEventListener('DOMContentLoaded',()=>{
 
+    const firstInput=+prompt('Enter first number');
+
+    console.log(sum(firstInput,5));
 });
 
 
-const firstInput=+prompt('Enter first number');
 
-console.log(sum(firstInput,5));
--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,4 +1,12 @@
-const webpackConfig={
+import {resolve,dirname} from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+// import {dirname} from 'path';
 
+const webpackConfig={
+    entry: './src/index.js',
+    output: {
+
+    }
 };
 export default webpackConfig;
