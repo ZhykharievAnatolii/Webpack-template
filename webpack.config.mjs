2e6a197112a6adad93270baa2fe799722ec46d2b
ZhykharievAnatolii <nekijt87@gmail.com>
2022-12-29 16:51:08 +0200

    @babel/plugin-transform-runtime
    for async and await

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -40,7 +40,8 @@ const webpackConfig={
                 use: {
                     loader: "babel-loader",
                     options: {
-                        presets: ['@babel/preset-env']
+                        presets: ['@babel/preset-env'],
+                        plugins:["@babel/plugin-transform-runtime"]
                     }
                 }
             }
